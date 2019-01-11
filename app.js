e72240b433d25193d2915e429aebdd71f28c6967
unknown <unknown@example.com>
2019-01-11 17:37:41 +0800

    小程序代码结构了解注释

--- app.js
+++ app.js
@@ -9,18 +9,30 @@ App({
     // 登录
     wx.login({
       success: res => {
+        // 获取登录凭证（res.code）
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({//code2Session
+          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
+          data: {
+            appid: 'wxa6ab3972bd6dad80',
+            secret: '403702da9aaa599d577f1f791fe37869',
+            js_code: res.code
+          },
+          success(res) {
+            console.log(res.data)
+          }
+        })
       }
     })
     // 获取用户信息
-    wx.getSetting({
+    wx.getSetting({//获取用户的当前设置
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+          wx.getUserInfo({//获取用户信息
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo//昵称、头像、性别、国家...
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -33,7 +45,7 @@ App({
       }
     })
   },
-  globalData: {
+  globalData: {//全局数据
     userInfo: null
   }
 })
